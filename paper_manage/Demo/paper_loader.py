aa6ba41959033265860b1f5af8f74632033fccd1
chLi <565353780@qq.com>
2023-12-19 17:11:41 +0800

    update paper data locations

--- paper_manage/Demo/paper_loader.py
+++ paper_manage/Demo/paper_loader.py
@@ -1,9 +1,9 @@
 from paper_manage.Module.paper_loader import PaperLoader
 
 def demo():
-    data_folder_path = '/home/chli/Nutstore Files/My-Materials/GCL年会/视频/毕业论文数据/'
-    wanfang_data_folder_path = data_folder_path + '万方/'
-    zhiwang_data_folder_path = data_folder_path + '知网/'
+    data_folder_path = '/home/chli/Nutstore Files/My-Materials/GCL年会/视频/毕业论文数据/Source/'
+    wanfang_data_folder_path = data_folder_path + 'Wanfang/'
+    zhiwang_data_folder_path = data_folder_path + 'Zhiwang/'
     ignore_same = True
 
     paper_loader = PaperLoader()
--- paper_manage/Demo/paper_manager.py
+++ paper_manage/Demo/paper_manager.py
@@ -1,9 +1,9 @@
 from paper_manage.Module.paper_manager import PaperManager
 
 def demo():
-    data_folder_path = '/home/chli/Nutstore Files/My-Materials/GCL年会/视频/毕业论文数据/'
-    wanfang_data_folder_path = data_folder_path + '万方/'
-    zhiwang_data_folder_path = data_folder_path + '知网/'
+    data_folder_path = '/home/chli/Nutstore Files/My-Materials/GCL年会/视频/毕业论文数据/Source/'
+    wanfang_data_folder_path = data_folder_path + 'Wanfang/'
+    zhiwang_data_folder_path = data_folder_path + 'Zhiwang/'
     ignore_same = True
     filter_mode = 'author'
     save_excel_file_path = '/home/chli/Nutstore Files/My-Materials/GCL年会/视频/毕业论文数据/all.xls'
@@ -18,7 +18,7 @@ def demo():
     return True
 
 def demo_acm():
-    data_folder_path = '/home/chli/Nutstore Files/My-Materials/GCL年会/视频/毕业论文数据/'
+    data_folder_path = '/home/chli/Nutstore Files/My-Materials/GCL年会/视频/毕业论文数据/Source/'
     acm_data_folder_path = data_folder_path + 'ACM/'
     ignore_same = True
     filter_mode = 'title'
